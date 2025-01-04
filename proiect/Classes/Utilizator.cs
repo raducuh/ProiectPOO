7d26566f432c9434742447df79ee44b16130f693
raducuh <raduponorean0223@gmail.com>
2025-01-04 20:32:37 +0200

    utilizator

--- proiect/Classes/Utilizator.cs
+++ proiect/Classes/Utilizator.cs
@@ -2,5 +2,18 @@
 
 public class Utilizator
 {
-    
+    public int ID { get; set; }
+    public string Nume { get; set; }
+    public string Prenume { get; set; }
+    public string Email { get; set; }
+    public string Parola { get; set; }
+
+    public Utilizator(int id, string nume, string prenume, string email, string parola)
+    {
+        ID = id;
+        Nume = nume;
+        Prenume = prenume;
+        
+
+    }
 }
