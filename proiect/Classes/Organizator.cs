628f45e13e02e1bc90137b626eb00348a08e8016
raducuh <raduponorean0223@gmail.com>
2025-01-04 20:37:43 +0200

    inceperea clasei organizator

--- proiect/Classes/Organizator.cs
+++ proiect/Classes/Organizator.cs
@@ -2,5 +2,11 @@
 
 public class Organizator:Utilizator
 {
+    public Organizator(int id, string nume, string prenume, string email, string parola) : base(id, nume, prenume,
+        email, parola)
+    {
+        
+    }
+    
     
 }
--- proiect/Program.cs
+++ proiect/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace proiect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
