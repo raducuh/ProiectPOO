b3be23e8846bb066621cd6e3436ea26c68f21f17
alexandrachiriac34 <alexandrachiriacc90@gmail.com>
2025-01-11 21:27:50 +0200

    am adaugat clasa Client

--- proiect/Classes/Client.cs
+++ proiect/Classes/Client.cs
@@ -1,7 +1,10 @@
 ﻿namespace DefaultNamespace;
 
-public class Organizator:Client(int id,string nume,string prenume,string email,string parola) : base(id, nume, prenume,
-        email, parola)
+public class Client : Utilizator
 {
-        
+    public Client(int id, string nume, string prenume, string email, string parola) : base(id, nume, prenume,
+        email, parola)
+    {
+
+    }
 }
