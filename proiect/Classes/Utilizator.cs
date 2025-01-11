62bf8e87a9672923326e20765810919faf44b9e7
raducuh <raduponorean0223@gmail.com>
2025-01-11 18:14:06 +0200

    rezolvare erori v2

--- proiect/Classes/Utilizator.cs
+++ proiect/Classes/Utilizator.cs
@@ -16,5 +16,6 @@ public class Utilizator
         Email = email;
         Parola= parola;
     }
-    
+
+ 
 }
--- proiect/Program.cs
+++ proiect/Program.cs
@@ -2,22 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 
+using DefaultNamespace;
+
+
 namespace proiect
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            var logger = new Logare();
+
             while (true)
             {
                 Console.WriteLine("1.Logare ");
-                Console.WriteLine("2. ceva  ");
+                Console.WriteLine("2.  ");
                 Console.WriteLine("3.  ");
                 Console.WriteLine("4. ");
                 Console.WriteLine("Alegeti o optiune: ");
                 int optiune= int.Parse(Console.ReadLine());
                 switch (optiune)
                 {
+
+                    case 1:
+                        logger.AddUtilizator();
+                        logger.AfisareUtilizatori();
+                        break;
+
                     
                 }
                 
